9e8cdfcc3dfac0f365fee9ddea65d7269c488029
Jerry Krinock <jerry@sheepsystems.com>
2013-09-19 10:12:34 -0700

    Initial Commit

---
+++ SSYWindowHangoutDemo/WHOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WHOAppDelegate.h
+//  SSYWindowHangoutDemo
+//
+//  Created by Jerry on 13/09/19.
+//  Copyright (c) 2013 Jerry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WHOAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SSYWindowHangoutDemo/WHOAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  WHOAppDelegate.m
+//  SSYWindowHangoutDemo
+//
+//  Created by Jerry on 13/09/19.
+//  Copyright (c) 2013 Jerry. All rights reserved.
+//
+
+#import "WHOAppDelegate.h"
+
+@implementation WHOAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
